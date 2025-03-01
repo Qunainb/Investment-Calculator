89909d5995a152c1affa3b90ba38350852d5867c
Qunainb <waniqunain@gmail.com>
2025-03-01 13:24:28 +0530

    Added Result Table

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import Header from "./Components/Header";
+import Results from "./Components/Results";
 import UserInput from "./Components/UserInput";
+import { useState } from "react";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 15000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevValues) => {
+      return {
+        ...prevValues,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      <Results input={userInput} />
     </>
   );
 }
